82c2012a6c90b038ce43a69b04250a216e18fd83
Konrad von Kügelgen <vkuegelgen@pc08.inb.uni-luebeck.de>
2020-05-06 15:16:18 +0200

    train and test, AMI dataset works

---
+++ classification_convertscript.py
@@ -0,0 +1,148 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import sys
+sys.path.append('../..')
+
+
+# %%
+from DLBio import pt_training
+import ds_ear
+from torchvision.models.mobilenet import mobilenet_v2
+import torch.nn as nn
+from DLBio.pytorch_helpers import get_device, get_num_params
+from DLBio.helpers import check_mkdir
+from DLBio.pt_train_printer import Printer
+import json
+import matplotlib.pyplot as plt
+from os.path import join
+
+
+# %%
+class Classification(pt_training.ITrainInterface):
+    def __init__(self, model, device):
+        self.model = model
+        self.xent_loss = nn.CrossEntropyLoss()
+        self.metrics = {
+            'acc': accuracy
+        }
+        self.d = device
+
+    def train_step(self, sample):
+        images, targets = sample[0].to(self.d), sample[1].to(self.d)
+        pred = self.model(images)
+
+        loss = self.xent_loss(pred, targets)
+        metrics = dict()
+
+        metrics.update({k: v(pred, targets) for k, v in self.metrics.items()})
+
+        return loss, metrics
+
+
+def accuracy(y_pred, y_true):
+    _, y_pred = y_pred.max(1)  # grab class predictions
+    return (y_pred == y_true).float().mean().item()
+
+
+# %%
+# define constants
+FOLDER = './class_sample'
+BATCH_SIZE = 16
+NUM_WORKERS = 4 # set to zero, if you want to run a debugger
+OPT_TYPE = 'Adam'
+LEARNING_RATE = 0.001
+WEIGHT_DECAY = 0.00001
+EPOCHS = 9
+LR_STEPS = 3
+DO_EARLY_STOPPING = True
+STOP_AFTER = 10
+ES_METRIC = 'val_acc'
+SAVE_INTERVALL = -1
+PRINT_FREQUENCY = 500 # print every 500 batches
+SEED = 0
+
+# use seeds to ensure comparable results ()
+pt_training.set_random_seed(SEED)
+
+
+# %%
+device = get_device()
+print(device)
+model = mobilenet_v2(pretrained=True)
+# cifar only has 10 classes
+model = model.to(device)
+#model = model.to('cuda:0')
+
+
+# %%
+
+
+check_mkdir(FOLDER, is_dir=True)
+model_out = join(FOLDER, 'model.pt')
+log_file = join(FOLDER, 'log.json')
+
+# write some model specs
+with open(join(FOLDER, 'model_specs.json'), 'w') as file:
+    json.dump({
+        'num_trainable': float(get_num_params(model, True)),
+        'num_params': float(get_num_params(model, False))
+    }, file)
+
+dl_train = ds_ear.get_dataloader(
+            is_train=True,
+            batch_size=BATCH_SIZE,
+            num_workers=NUM_WORKERS
+        )
+
+dl_test = ds_ear.get_dataloader(
+            is_train=False,
+            batch_size=BATCH_SIZE,
+            num_workers=NUM_WORKERS
+        )
+
+optimizer = pt_training.get_optimizer(
+        'Adam', model.parameters(),
+        LEARNING_RATE,
+        weight_decay=WEIGHT_DECAY
+    )
+
+if LR_STEPS > 0:
+    scheduler = pt_training.get_scheduler(
+            LR_STEPS, EPOCHS, optimizer)
+
+if DO_EARLY_STOPPING:
+    assert SAVE_INTERVALL == -1
+    early_stopping = pt_training.EarlyStopping(
+            ES_METRIC, get_max=True, epoch_thres=STOP_AFTER
+        )
+else:
+    early_stopping = None
+    
+train_interface = Classification(model, device)
+
+training = pt_training.Training(
+        optimizer, dl_train, train_interface,
+        scheduler=scheduler, printer=Printer(PRINT_FREQUENCY, log_file),
+        save_path=model_out, save_steps=SAVE_INTERVALL,
+        val_data_loader=dl_test,
+        early_stopping=early_stopping
+    )
+
+training(EPOCHS)
+
+
+# %%
+with open(log_file, 'r') as file:
+    log = json.load(file)
+
+plt.plot(log['acc'], label='acc')
+plt.plot(log['val_acc'], label='val_acc')
+
+plt.legend()
+plt.grid()
+
+
+# %%
+
+
--- ds_ear.py
+++ ds_ear.py
@@ -1,8 +1,11 @@
 import torchvision
 from torch.utils.data import DataLoader, SubsetRandomSampler, Dataset
+import numpy as np
+from os.path import join, dirname
+import os
 
 
-DATA_FOLDER = 'dataset'
+DATA_FOLDER = join(dirname(os.getcwd()),'AMI')
 RESIZE_M = 150
 RESIZE_N = 100
 
@@ -10,27 +13,53 @@ def get_dataloader(
     is_train=True, indeces=None, batch_size=32, num_workers=0,
         data_path=DATA_FOLDER):
     # indeces, if you only want to train a subset
-    dataset = get_dataset(is_train, data_path)
+    dataset = get_dataset(data_path)
 
-    if indeces is None:
+    # Split dataset into fix train and test
+    train_size = 0.6
+    dataset_size = len(dataset)
+    # split all indices at train percentage
+    indices = list(range(dataset_size))
+    split = int(np.floor(train_size * dataset_size))
+    np.random.seed(0)
+    np.random.shuffle(indices)
+
+    train_indices, test_indices = indices[:split], indices[split:]
+    # create samplers
+    train_sampler = SubsetRandomSampler(train_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+    
+    if is_train:
         data_loader = DataLoader(
             dataset,
-            batch_size=batch_size,
-            shuffle=is_train,
-            num_workers=num_workers
-        )
+            batch_size=batch_size,#args.batch_size,
+            num_workers=num_workers,#args.loader_num_workers,
+            sampler=train_sampler)
     else:
         data_loader = DataLoader(
             dataset,
-            batch_size=batch_size,
-            shuffle=is_train,
-            num_workers=num_workers,
-            sampler=SubsetRandomSampler(indeces)
-        )
+            batch_size=batch_size,#args.batch_size,
+            num_workers=num_workers,#args.loader_num_workers,
+            sampler=test_sampler)
+
+    # if indeces is None:
+    #     data_loader = DataLoader(
+    #         dataset,
+    #         batch_size=batch_size,
+    #         shuffle=is_train,
+    #         num_workers=num_workers
+    #     )
+    # else:
+    #     data_loader = DataLoader(
+    #         dataset,
+    #         batch_size=batch_size,
+    #         shuffle=is_train,
+    #         num_workers=num_workers,
+    #         sampler=SubsetRandomSampler(indeces)
+    #     )
     return data_loader
 
-
-def get_dataset(is_train=True, data_path=DATA_FOLDER):
+def get_dataset(data_path=DATA_FOLDER):
     dataset = torchvision.datasets.ImageFolder(
         root=data_path,
         transform=torchvision.transforms.Compose([
@@ -45,7 +74,5 @@ def get_dataset(is_train=True, data_path=DATA_FOLDER):
         ])
         
     )
-    print("helloow")
     print(dataset.classes)
-
     return dataset
