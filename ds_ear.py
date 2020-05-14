79b99d9b552ee5553a2e61bf7f21eb12c431e0f8
Konrad von Kügelgen <vkuegelgen@pc08.inb.uni-luebeck.de>
2020-05-14 15:46:21 +0200

    add central ownDataset-AMI swap variables

--- ds_ear.py
+++ ds_ear.py
@@ -5,7 +5,7 @@ from torchvision import transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader, SubsetRandomSampler, Dataset
 
-DATA_FOLDER = '../dataset'
+DATA_FOLDER = '../AMI'
 RESIZE_Y = 150
 RESIZE_X = 100
 
@@ -31,22 +31,22 @@ def get_dataloader(indeces=None, batch_size=32, num_workers=0, is_train = True, 
     return data_loader
 
 
-def get_dataset(data_path=DATA_FOLDER, is_train=True):
+def get_dataset(data_path=DATA_FOLDER, is_train=False):
 
     transform_dict = {
         'train': td.transforms_train(
                     (RESIZE_Y, RESIZE_X),
-                    [0.49139968, 0.48215841, 0.44653091],
-                    # alternate mean: [0.485, 0.456, 0.406],
-                    [0.24703223, 0.24348513, 0.26158784]
-                    # alternate std: [0.229, 0.224, 0.225]
+                    # [0.49139968, 0.48215841, 0.44653091],
+                    [0.485, 0.456, 0.406], # imageNet mean
+                    # [0.24703223, 0.24348513, 0.26158784]
+                    [0.229, 0.224, 0.225] # imageNet std
                     ),
         'valid_and_test': td.transforms_valid_and_test(
                     (RESIZE_Y, RESIZE_X),
-                    [0.49139968, 0.48215841, 0.44653091],
-                    # alternate mean: [0.485, 0.456, 0.406],
-                    [0.24703223, 0.24348513, 0.26158784]
-                    # alternate std: [0.229, 0.224, 0.225]
+                    # [0.49139968, 0.48215841, 0.44653091],
+                    [0.485, 0.456, 0.406], # imageNet mean
+                    # [0.24703223, 0.24348513, 0.26158784]
+                    [0.229, 0.224, 0.225] # imageNet std
                     )
         }
     
