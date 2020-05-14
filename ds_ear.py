f0d6beb6c62228eff054499603729fc2e7e3ee22
Falco Lentzsch <lentzsch@pc09.inb.uni-luebeck.de>
2020-05-14 13:02:55 +0200

    first working version of classification#

--- ds_ear.py
+++ ds_ear.py
@@ -54,5 +54,4 @@ def get_dataset(data_path=DATA_FOLDER, is_train=True):
                     root = data_path,
                     transform=transform_dict['train' if is_train else 'valid_and_test']
                     ) 
-
     return dataset
