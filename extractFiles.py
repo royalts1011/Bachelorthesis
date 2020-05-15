8216c8eb74b2c1554f61b23c104e7ae9c7535ef4
Konrad von Kügelgen <vkuegelgen@pc08.inb.uni-luebeck.de>
2020-05-15 12:32:37 +0200

    now working

--- extractFiles.py
+++ extractFiles.py
@@ -1,37 +1,44 @@
 import os
-from os.path import splitext, split, dirname, join
+from os.path import splitext, split, dirname, join, isdir
 
 ########################################################################
-# Copy this script into the dataset directory, e.g "konrad_von"
-# and execute it
-# A directory in the parent folder will be created and pictures be moved.
+# Copy this script to dataset directory, containing all folders of images
+# and execute it.
+# Type in the target folder and a directory 
+# in the parent folder will be created and pictures be moved.
 # Make sure pictures_to_move and the step are valid entries
 ########################################################################
-# This moves picture 001, 010, 020, ..., 070
-pictures_to_move = 8 
+# This moves picture 001, 011, 021, ..., 071
+pictures_to_move = 10 
 step = 10
 ########################################################################
 
-
-
+targ_folder = input("Please input the name of the target folder: \n")
 cwd = os.getcwd()
-new_dir = split(cwd)[0]
-dirs = os.listdir(cwd)
+
+while not isdir(join(cwd, targ_folder)):
+    print("\nFolder does not exist.")
+    targ_folder = input("Please input the name of the target folder: \n")
+
+targ_dir = join(cwd, targ_folder)
+
+dirs = os.listdir(targ_dir)
+dirs.sort()
 # assert valid entries for the set parameters
-assert (pictures_to_move-1) * step < len(dirs)
-file_name = splitext(dirs[5])[0]
+assert (pictures_to_move-1) * step < len(dirs), "Amount of images in the folder won't fit with the parameters!"
+file_name = splitext(dirs[0])[0]
 folder = file_name[:len(file_name)-3] + "-testset"
 
 print("The folder ", folder, " will be created in following directory:")
-print(new_dir)
+print(cwd)
 input("If that is ok, press enter. Otherwise stop! (Ctrl+C)" + '\n')
 print("Ok, creating folder and moving images...")
 
-new_dir = join(new_dir, folder)
+new_dir = join(cwd, folder)
 os.mkdir(new_dir)
 
 for i in range(pictures_to_move):
     i *= step
-    os.rename(join(cwd, dirs[i]), join(cwd, folder))
+    os.rename(join(targ_dir, dirs[i]), join(new_dir, dirs[i]))
 
 print(pictures_to_move, " pictures were moved to ", new_dir)
