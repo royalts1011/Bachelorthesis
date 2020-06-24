2fe2fb5ac28808ab8e53da3e18d948dfcc2a1638
Falco Lentzsch <lentzsch@pc09.inb.uni-luebeck.de>
2020-06-24 12:13:23 +0200

    script for home training

--- ContrastiveLossFunction.py
+++ ContrastiveLossFunction.py
@@ -4,6 +4,9 @@ import torch.nn.functional as NNF
 from torch import optim
 import torch
 import numpy as np
+
+import struct
+import sys
 class ContrastiveLoss(torch.nn.Module):
     """
     Contrastive loss function.
@@ -14,10 +17,11 @@ class ContrastiveLoss(torch.nn.Module):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
 
+
     def forward(self, output1, output2, label):
         euclidean_distance = NNF.pairwise_distance(output1, output2, keepdim = True)
         loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
                                       (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
 
-
         return loss_contrastive
+    
