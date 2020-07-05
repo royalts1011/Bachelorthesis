4b79088b8062e888bd0edf40d17753ba369ebb65
Konrad <konrad.vonkuegelgen@student.uni-luebeck.de>
2020-07-05 14:43:35 +0200

    ROC

--- Siamese_MobileNetV2_Train.py
+++ Siamese_MobileNetV2_Train.py
@@ -3,68 +3,87 @@
 # %%
 import sys
 sys.path.append('../..')
-from torchvision.utils import make_grid
-from torchvision.models.mobilenet import mobilenet_v2
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-import torch
-import torchvision
-from DLBio.pytorch_helpers import get_device
 import numpy as np
 import matplotlib.pyplot as plt
+
+# PyTorch
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import cuda
+from torch.autograd import Variable
+from torchvision.utils import make_grid
+from torchvision.models.mobilenet import mobilenet_v2
+
+# DLBio and own scripts
+from DLBio.pytorch_helpers import get_device
 import ds_ear_siamese
 import transforms_data as td
+from helpers import cuda_conv
+import metrics as M
 from siamese_network_train import Training
 from ContrastiveLossFunction import ContrastiveLoss
-from torch import cuda
 from NN_Siamese import SiameseNetwork
 
 
 # %%
-# def imshow(img,text=None,should_save=False):
-#     npimg = img.numpy()
-#     plt.axis("off")
-#     if text:
-#         plt.text(75, 8, text, style='italic',fontweight='bold',
-#             bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
-#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-#     plt.show()    
+def imshow(img,text=None,should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic',fontweight='bold',
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()    
 
-# def show_plot(iteration,loss):
-#     plt.plot(iteration,loss)
-#     plt.show()
+def show_plot(epochs, value1, value2, label1, label2, plt_number):
+    plt.figure(plt_number)
+    plt.plot(epochs,value1, label=label1)
+    plt.plot(epochs,value2, label=label2)
+    plt.legend()
+    plt.grid()
 
 
 # %%
 # Set Up All Configurations here
 class Config():
+    #1. Boolean ändern
+    #2. FC-Layer auf Bild anpassen
+    #3. LR auf 0,0005
     NN_SIAMESE = False
-    dataset_dir = '../dataset_low_res/'
+    dataset_dir = '../dataset/'
     # training_dir = "../data/ears/training/"
     # testing_dir = "../data/ears/testing/"
     train_batch_size = 32
-    test_batch_size = 1
+    val_batch_size = 32
+    test_batch_size = 16
     vis_batch_size = 8
     num_workers = 3
     
-    EPOCHS= 50
+    EPOCHS= 5
     LEARNINGRATE = 0.001
     WEIGHT_DECAY = 0
+
+    TRESHOLD_VER = 0.8
+    a = 0
 
 
 # %%
 # define indicies to split Data
 N = len(ds_ear_siamese.get_dataset(data_path=Config.dataset_dir, transform_mode='size_only'))
 print(N)
-n_90 = int(round(.9*N))
 n_80 = int(round(.8*N))
 n_70 = int(round(.7*N))
 n_60 = int(round(.6*N))
+n_20 = int(round(.2*N))
+n_10 = int(round(.1*N))
 
 rand_indices = np.random.permutation(N)
 train_indices = rand_indices[:n_70]
-test_indices = rand_indices[n_70:]
+val_indices = rand_indices[n_70:n_70+n_20]
+test_indices = rand_indices[n_70+n_20:]
+
 
 # definde data loader
 # dl_train = ds_ear_siamese.get_dataloader(
@@ -77,34 +96,42 @@ train_dataloader = ds_ear_siamese.get_dataloader(
     should_invert = False
 )
 
+val_dataloader = ds_ear_siamese.get_dataloader(
+    data_path=Config.dataset_dir,
+    indices=val_indices,
+    batch_size=Config.val_batch_size,
+    num_workers=Config.num_workers,
+    transform_mode='siamese_valid_and_test',
+    should_invert = False
+)
 # dl_test = ds_ear_siamese.get_dataloader(
 test_dataloader = ds_ear_siamese.get_dataloader(
     data_path=Config.dataset_dir,
     indices=test_indices,
     batch_size=Config.test_batch_size,
     num_workers=Config.num_workers,
+    transform_mode='siamese_valid_and_test',
+    should_invert = False
+)
+
+vis_dataloader = ds_ear_siamese.get_dataloader(
+    data_path=Config.dataset_dir,
+    indices=train_indices,
+    batch_size=Config.vis_batch_size,
+    num_workers=Config.num_workers,
     transform_mode='siamese',
     should_invert = False
 )
 
-# vis_dataloader = ds_ear_siamese.get_dataloader(
-#     data_path=Config.dataset_dir,
-#     indices=train_indices,
-#     batch_size=Config.vis_batch_size,
-#     num_workers=Config.num_workers,
-#     transform_mode='siamese',
-#     should_invert = False
-# )
-
 
 # %%
 # visualize some data....
-# dataiter = iter(vis_dataloader)
+dataiter = iter(vis_dataloader)
 
-# example_batch = next(dataiter)
-# concatenated = torch.cat((example_batch[0], example_batch[1]),0)
-# imshow(make_grid(concatenated))
-# print(example_batch[2].numpy())
+example_batch = next(dataiter)
+concatenated = torch.cat((example_batch[0], example_batch[1]),0)
+imshow(make_grid(concatenated))
+print(example_batch[2].numpy())
 
 
 # %%
@@ -112,15 +139,22 @@ test_dataloader = ds_ear_siamese.get_dataloader(
 if Config.NN_SIAMESE == False:
     model = mobilenet_v2(pretrained=True)
     model.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-    model.classifier[1] = nn.Linear(in_features=model.classifier[1].in_features, out_features=5)
-
+    #model.classifier[1] = nn.Linear(in_features=model.classifier[1].in_features, out_features=10)
+    
     layers = []
+    
     for layer in model.features[0]:
         layers.append(layer)
     model.features[0][0] = nn.ReflectionPad2d(1)
     model.features[0][1] = layers[0]
     model.features[0][2] = layers[1]
     model.features[0].add_module('3', layers[2])
+    
+    model.classifier = nn.Sequential(*list(model.classifier.children())[:-2])
+
+    #model = torch.nn.Sequential(*(list(model.children()[:-1]))
+    #model = model.features
+
 else:
     model = SiameseNetwork()
 
@@ -129,74 +163,108 @@ device = get_device()
 print(device)
 model.to(device)
 
-contrastive_loss_siamese = ContrastiveLoss()
+contrastive_loss_siamese = ContrastiveLoss(2.0)
 optimizer_siamese = torch.optim.Adam(model.parameters(),lr = Config.LEARNINGRATE)
 
 
 # %%
-training = Training(model=model, optimizer=optimizer_siamese,train_dataloader=train_dataloader, 
-                loss_contrastive=contrastive_loss_siamese, nn_Siamese=Config.NN_SIAMESE)
+#To Define which Layers we want to train
+for param in model.parameters():
+    param.requires_grad = False
 
-counter, loss_history = training(Config.EPOCHS)
-# show_plot(counter, loss_history)
-
-
-# %%
-if Config.NN_SIAMESE:
-    def generate_output( img_in, img_in2):
-        if cuda.is_available():
-            return model(Variable(img_in).cuda(), Variable(img_in2).cuda())
-        else:
-            return model(Variable(img_in), Variable(img_in2))
-
-else:
-    def generate_output( img_in):
-        if cuda.is_available():
-            return model(Variable(img_in).cuda())
-        else:
-            return model(Variable(img_in))         
+layers = list(model.children())[0]
+sub_layer = list(layers.children())
+unfreezed = [15,16,17,18]
+for u in unfreezed:
+    for param in sub_layer[u].parameters():
+        param.requires_grad = True
 
 
 # %%
-iter_batches = iter(test_dataloader)
-accuracy_count = 0.0
-matches = []
-non_matches = []
-for i in range(len(iter_batches)):
-    # batch size schould be 1 to throw no errors in accessing the next(iter_batches) 
-    img0, img1, label = next(iter_batches)
-    # appends images to matches, if label is 0 (0 = same)
-    ((matches, non_matches)[int(label)]).append( (img0,img1) )
+# To show trainable parameters
+from DLBio.pytorch_helpers import get_num_params
 
-# iterate through both lists and compare the distances of the current entries
-for m, n in zip(matches, non_matches):
-    # get the models output vectors for the images
-    
-    if Config.NN_SIAMESE:
-        match_out1, match_out2 = generate_output(m[0], m[1])
-        non_match_out1, non_match_out2 = generate_output(n[0], n[1])
-    else:
-        match_out1 = generate_output(m[0])
-        match_out2 = generate_output(m[1])                 
-        non_match_out1 = generate_output(n[0])
-        non_match_out2 = generate_output(n[1])
-        
-    # positive-positive, positive-negative distance
-    euclidean_distance_pp = F.pairwise_distance(match_out1, match_out2)
-    euclidean_distance_pn = F.pairwise_distance(non_match_out1, non_match_out2)
-    
-    # format variables
-    fmt_id = '{:<12}'
-    fmt_eucl = '{:<.3f}'
-    # increase accouracy count if p-p was lower than p-n
-    if(euclidean_distance_pp < euclidean_distance_pn): accuracy_count += 1
-    # print current stats
-    print(fmt_id.format('pos-pos: '), fmt_eucl.format( euclidean_distance_pp.item()) )
-    print(fmt_id.format('pos-neg: '),fmt_eucl.format( euclidean_distance_pn.item()) )
-    print(fmt_id.format('Acc. count: '), '{:>.0f}'.format(accuracy_count), '\n')
+get_num_params(model,True)
 
-# divide by the minimum length (zip function regulated the steps to the minimum)
-print(fmt_id.format('Overall (estimated) accuracy: '), fmt_eucl.format( 100 * accuracy_count / min(len(matches), len(non_matches)) ), ' %')
+
+# %%
+training = Training(model=model, optimizer=optimizer_siamese,train_dataloader=train_dataloader, val_dataloader=val_dataloader, loss_contrastive=contrastive_loss_siamese, nn_Siamese=Config.NN_SIAMESE, THRESHOLD=Config.TRESHOLD_VER)
+
+epochs, loss_history, val_loss_history, acc_history, val_acc_history = training(Config.EPOCHS)
+# show_plot(epochs, loss_history, val_loss_history,'train_loss', 'val_loss',1)
+# show_plot(epochs, acc_history, val_acc_history,'train_acc', 'val_acc', 2)
+
+
+# %%
+# tn Bilder nicht gleich, Distanz größer als THRESH
+# fp Bilder nicht gleich, Distanz kleiner als THRESH
+# fn Bilder gleich, Distanz größer als THRESH
+# tp Bilder gleich, Distanz kleiner als THRESH
+
+def calc_test_label(thresh=Config.TRESHOLD_VER):
+    '''
+    This function processes the test dataloader and returns the true labels and the predicted labels (depending on a threshold)
+    Arguments
+    ---------
+    thresh:     Threshold for "same-different" classification
+                default is the Config set threshhold
+                
+
+    Returns
+    ---------
+    Two lists of same length as image tuples in test loader with labels 1 or 0
+    '''
+    ground_truth_label, prediction_label = [], []
+
+    for data in test_dataloader:
+        # use training class for data extraction
+        label, output1, output2 = training.get_label_outputs(data)
+        # extend labels of the ground truth
+        ground_truth_label.extend(label.flatten().tolist())
+        # Extend the distance-threshold prediction
+        prediction_label.extend(M.batch_predictions_bin(output1, output2, thresh))
+    
+    return ground_truth_label, prediction_label
+
+ground_truth, prediction = calc_test_label(Config.TRESHOLD_VER)
+# get confusion matrix
+cf = M.cf_matrix(ground_truth, prediction)
+print(cf)
+
+
+# %%
+# Set parameters for confusion_matrix plot
+labels = ['True Pos','False Neg','False Pos','True Neg']
+categories = ['Same', 'Different']
+
+# plot matrix
+# M.make_confusion_matrix(cf,
+#                         group_names=labels,
+#                         categories=categories,
+#                         cbar=True
+#                         )
+
+
+# %%
+# preparation for ROC
+# define lists for the rates
+tprs = []
+fprs = []
+# Set all Thresholds to be tested
+threshholds = np.arange(start=0.0, stop=1.2, step=0.1)
+
+for t in threshholds:
+    ground_truth, prediction = calc_test_label(t)
+    cf = M.cf_matrix(ground_truth, prediction)
+    print(cf)
+    _,_,_,sensitivity,specificity = M.get_metrics(cf)
+
+    tprs.extend(sensitivity)
+    fprs.extend( (1 - specificity) )
+
+print("\n TPRS:\t", tprs)
+print(" FPRS:\t", fprs)
+# show_plot(fprs, tprs, tprs, "ROC", "ROC2", 3)
 
 
 # %%
@@ -204,7 +272,7 @@ print(fmt_id.format('Overall (estimated) accuracy: '), fmt_eucl.format( 100 * ac
 
 
 # %%
-torch.save(model,'/nfshome/lentzsch/Documents/Bachelorarbeit/Bachelorthesis/models/model.pt')
+#torch.save(model,'/nfshome/lentzsch/Documents/Bachelorarbeit/Bachelorthesis/models/model_MN_1.pt')
 
 
 # %%
